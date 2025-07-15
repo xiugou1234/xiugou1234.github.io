08b0ef33d6494b6b56673a1bc9c606ffab2f39f7
xiugou1234 <2596639431@qq.com>
2025-07-15 15:50:26 +0800

    Site updated: 2025-07-15 15:50:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/15/这是一篇新的博文/","2025/07/15/hello-world/"];function toRandomPost(){
+var posts=["2025/07/15/这是一篇新的博文-1/","2025/07/15/这是一篇新的博文/","2025/07/15/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
